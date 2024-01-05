53808a3eb0062d3167f5524b3602afe806e7d0aa
seojeewon <readyjiwon@gmail.com>
2024-01-05 18:06:53 +0900

    fix: car image entity add not null

--- src/main/java/com/example/kingcar_be/Entity/CarImage.java
+++ src/main/java/com/example/kingcar_be/Entity/CarImage.java
@@ -1,18 +1,23 @@
 package com.example.kingcar_be.Entity;
 
 import jakarta.persistence.*;
+import org.antlr.v4.runtime.misc.NotNull;
+
 
 @Entity
 public class CarImage {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long carImageId;
 
+    @NotNull
     @Column(name = "brand_name")
     private String brandName;
 
+    @NotNull
     @Column(name = "brand_model")
     private String brandModel;
 
+    @NotNull
     @Column(name = "model_image")
     private String modelImage;
 }
