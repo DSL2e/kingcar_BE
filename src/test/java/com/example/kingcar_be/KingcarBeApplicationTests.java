f8a70e1d4d2a13e2e357e7fef6fcfd0fd5556950
admin <admin@adminui-MacBookPro-10.local>
2024-01-05 16:11:11 +0900

    initial commit

---
+++ src/test/java/com/example/kingcar_be/KingcarBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kingcar_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KingcarBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
