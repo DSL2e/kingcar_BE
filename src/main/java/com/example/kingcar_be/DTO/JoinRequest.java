4edc371fb6c864b1cd7b040c3f1703fd599489aa
seojeewon <readyjiwon@gmail.com>
2024-01-06 20:15:42 +0900

    docs: update .gitignore
    exclude application.yml

--- src/main/java/com/example/kingcar_be/DTO/JoinRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.kingcar_be.DTO;
-
-import lombok.Getter;
-
-@Getter
-public class JoinRequest {
-    private String nickname;
-}
--- src/main/java/com/example/kingcar_be/Repository/JoinRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.kingcar_be.Repository;
-
-import com.example.kingcar_be.Entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JoinRepository extends JpaRepository<Member, Long> {
-}
