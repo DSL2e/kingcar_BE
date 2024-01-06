9967c8f6f4359aa66fcefc2506706c1702dda6ff
seojeewon <readyjiwon@gmail.com>
2024-01-06 22:12:46 +0900

    test: add log when join success

--- src/main/java/com/example/kingcar_be/Controller/JoinController.java
+++ src/main/java/com/example/kingcar_be/Controller/JoinController.java
@@ -5,6 +5,7 @@ import com.example.kingcar_be.DTO.MemberIdResponse;
 import com.example.kingcar_be.DTO.Status;
 import com.example.kingcar_be.Service.JoinService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,11 +14,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController  //회원가입
 @RequiredArgsConstructor
+@Slf4j
 public class JoinController {
     private final JoinService joinService;
     @PostMapping("/members")
     public ResponseEntity<MemberIdResponse> join(@RequestBody JoinRequest request){
-        return ResponseEntity.status(201).body(joinService.join(request.getNickname()));
+        MemberIdResponse response = joinService.join(request.getNickname());
+        log.info("[회원가입 성공] ID: {}, NICKNAME: {}", response.getMemberId(), request.getNickname());
+        return ResponseEntity.status(201).body(response);
     }
 
     /*
