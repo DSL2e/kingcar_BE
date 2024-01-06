11838e99ce074dc7775eeecb6a170788ea587843
seojeewon <readyjiwon@gmail.com>
2024-01-06 20:16:47 +0900

    update gitignore

---
+++ src/main/java/com/example/kingcar_be/DTO/Status.java
@@ -0,0 +1,13 @@
+package com.example.kingcar_be.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class Status {
+    private String status;
+}
---
+++ src/main/java/com/example/kingcar_be/KingcarBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kingcar_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KingcarBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KingcarBeApplication.class, args);
+    }
+
+}
