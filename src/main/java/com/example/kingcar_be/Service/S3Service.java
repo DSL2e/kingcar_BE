4d137f28bfd6124405fbc18f692b73bfd4d596d0
seojeewon <readyjiwon@gmail.com>
2024-01-08 09:41:55 +0900

    feat: s3파일명에 난수 추가

--- src/main/java/com/example/kingcar_be/Service/S3Service.java
+++ src/main/java/com/example/kingcar_be/Service/S3Service.java
@@ -10,6 +10,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.IIOException;
 import java.io.IOException;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -21,10 +22,11 @@ public class S3Service {
 
     public String upload(MultipartFile file) throws IOException {
         String originalFilename = file.getOriginalFilename();
+        String randomNum = UUID.randomUUID().toString();    //파일명에 붙은 난수
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentLength(file.getSize());
         metadata.setContentType(file.getContentType());
-        amazonS3.putObject(bucket+"/upload", originalFilename, file.getInputStream(), metadata);
-        return amazonS3.getUrl(bucket+"/upload", originalFilename).toString();
+        amazonS3.putObject(bucket+"/upload", randomNum+originalFilename, file.getInputStream(), metadata);
+        return amazonS3.getUrl(bucket+"/upload", randomNum+originalFilename).toString();
     }
 }
