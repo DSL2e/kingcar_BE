e0e4bc15f1fb48975b68ad6834eaa389ad5993a7
admin <admin@adminui-MacBookPro-10.local>
2024-01-08 18:19:51 +0900

    feat:중복 닉네임 체크

--- src/main/java/com/example/kingcar_be/Service/JoinService.java
+++ src/main/java/com/example/kingcar_be/Service/JoinService.java
@@ -3,6 +3,8 @@ package com.example.kingcar_be.Service;
 import com.example.kingcar_be.DTO.MemberIdResponse;
 import com.example.kingcar_be.DTO.Status;
 import com.example.kingcar_be.Entity.Member;
+import com.example.kingcar_be.Exception.ErrorCode;
+import com.example.kingcar_be.Exception.NameDuplicationException;
 import com.example.kingcar_be.Repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,10 @@ import org.springframework.stereotype.Service;
 public class JoinService {
     private final MemberRepository memberRepository;
     public MemberIdResponse join(String nickname){
+
+        if(memberRepository.existsByNickname(nickname))
+            throw new NameDuplicationException(ErrorCode.INVALID_REQUEST,"중복된 닉네임입니다");
+
         Member newMember = new Member(nickname);
         memberRepository.save(newMember);
         return MemberIdResponse.builder().memberId(newMember.getMemberId()).build();
