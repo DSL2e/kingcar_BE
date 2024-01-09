29701032074d1fd5da10cc61fd43dd8bf04f5722
admin <admin@adminui-MacBookPro-26.local>
2024-01-09 14:57:58 +0900

    fix:상세페이지 dto수정

--- src/main/java/com/example/kingcar_be/DTO/ArticleDetail.java
+++ src/main/java/com/example/kingcar_be/DTO/ArticleDetail.java
@@ -14,16 +14,17 @@ import java.util.Optional;
 @Setter
 @AllArgsConstructor
 public class ArticleDetail {
-    private Long articleId; //게시물 Id
-    private String title;   //게시물 제목
-    private String nickname;  //게시물 작성자
-    private String image;   // 게시물 이미지
-    private String contents;     // 게시글 내용
-    private boolean connection;     //차주와 시승자 연결 여부
+    private Long articleId;         // 게시물 Id
+    private String title;           // 게시물 제목
+    private String nickname;        // 게시물 작성자
+    private String image;           // 게시물 이미지
+    private String contents;        // 게시글 내용
+    private boolean connection;     // 차주와 시승자 연결 여부
     private Long courseId;          // 코스
-    private int price;              //차주가 설정한 금액
-    private String writerCarBrand;  //차주 자동차 브랜드
-    private String writerCarModel;  //차주 자동차 모델
+    private int price;              // 차주가 설정한 금액
+    private String writerCarBrand;  // 차주 자동차 브랜드
+    private String writerCarModel;  // 차주 자동차 모델
+    private int RequestCount;        // 요청한 사람수
 
     public ArticleDetail(Article entity) {
         this.articleId = entity.getArticleId();
@@ -36,5 +37,6 @@ public class ArticleDetail {
         this.price = entity.getPrice();
         this.writerCarBrand=entity.getWriter().getCarBrand();
         this.writerCarModel=entity.getWriter().getCarModel();
+        this.RequestCount = entity.getRequests().size();
     }
 }
